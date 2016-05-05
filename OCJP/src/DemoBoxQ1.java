46ee958973c72869a331fc7489a8d6e05a8b5cd4
VBANDLA <layani.bandaranayake@pearson.com>
2016-05-05 06:48:49 +0530

    Initial Commit

---
+++ OCJP/src/DemoBoxQ1.java
@@ -0,0 +1,20 @@
+
+public class DemoBoxQ1 {
+	public static void main(String args[]){
+		int length;
+		int width;
+		int height;
+		int Height; //Java Is case sensitive 
+		
+		length = 12;
+		width = 5;
+		height = 3;
+		Height = 23;
+		
+		int volume;
+		volume = length*width*height;
+		System.out.println("Volume : " + volume);
+		
+	}
+
+}
